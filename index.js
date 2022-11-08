84accc464758afa25559c6648f00d893788c9ed0
heavenwebdev <georgecupid1126@gmail.com>
2022-11-07 23:39:20 -0800

    initialize

---
+++ index.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const mqtt = require('mqtt')
+const fs = require('fs')
+const { Command } = require('commander')
+
+const program = new Command()
+
+const serverport = 2021;
+
+const app = express();
+app.get('/', (req, res) => {
+    res.send('A Simple Node.js Server is Up and Running.......')
+})
+app.listen(serverport, ()=>{
+    console.log(`Server is listening on port ${serverport}`)
+});
+
+
+
+program
+  .option('-p, --protocol <type>', 'connect protocol: mqtt, mqtts, ws, wss. default is mqtt', 'mqtt')
+  .parse(process.argv)
+
+const host = '101.37.148.19'
+const port = '1883'
+const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
+
+// connect options
+const OPTIONS = {
+  clientId,
+  clean: true,
+  connectTimeout: 4000,
+  username: 'serverclient',
+  password: 'password',
+  reconnectPeriod: 1000,
+}
+// protocol list
+const PROTOCOLS = ['mqtt', 'mqtts', 'ws', 'wss']
+
+// default is mqtt, unencrypted tcp connection
+let connectUrl = `mqtt://${host}:${port}`
+if (program.protocol && PROTOCOLS.indexOf(program.protocol) === -1) {
+  console.log('protocol must one of mqtt, mqtts, ws, wss.')
+} else if (program.protocol === 'mqtts') {
+  // mqtts， encrypted tcp connection
+  connectUrl = `mqtts://${host}:8883`
+} else if (program.protocol === 'ws') {
+  // ws, unencrypted WebSocket connection
+  const mountPath = '/mqtt' // mount path, connect emqx via WebSocket
+  connectUrl = `ws://${host}:8083${mountPath}`
+} else if (program.protocol === 'wss') {
+  // wss, encrypted WebSocket connection
+  const mountPath = '/mqtt' // mount path, connect emqx via WebSocket
+  connectUrl = `wss://${host}:8084${mountPath}`
+  
+} else {}
+
+const topic = 'TEST-999666'
+console.log(connectUrl);
+const client = mqtt.connect(connectUrl, OPTIONS)
+
+client.on('connect', () => {
+  console.log(`${program.protocol}: Connected`)
+  client.subscribe([topic], () => {
+    console.log(`${program.protocol}: Subscribe to topic '${topic}'`)
+  })
+})
+
+client.on('reconnect', (error) => {
+  console.log(`Reconnecting(${program.protocol}):`, error)
+})
+
+client.on('error', (error) => {
+  console.log(`Cannot connect(${program.protocol}):`, error)
+})
+
+client.on('message', (topic, payload) => {
+  console.log('Received Message:', topic, payload.toString())
+})
+
+
+app.get('/turnon', (req, res) => {
+  client.publish(topic,"{'a':1}");
+  res.send('success')
+})
+app.get('/turnoff', (req, res) => {
+  client.publish(topic,"{'a':3}");
+  res.send('success')
+})
