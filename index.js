4c0ad828671f2de029ca9ea8664b2d5b465bcde8
heavenwebdev <georgecupid1126@gmail.com>
2022-11-10 04:27:27 -0500

    Initial backend

--- index.js
+++ index.js
@@ -17,9 +17,6 @@ app.use(cors({
   methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
 }));
 
-app.get('/', (req, res) => {
-    res.send('A Simple Node.js Server is Up and Running.......')
-})
 app.listen(serverport, ()=>{
     console.log(`Server is listening on port ${serverport}`)
 });
@@ -30,6 +27,8 @@ program
   .option('-p, --protocol <type>', 'connect protocol: mqtt, mqtts, ws, wss. default is mqtt', 'mqtt')
   .parse(process.argv)
 
+// const host = '54.210.189.224'
+// const port = '8083'
 const host = '101.37.148.19'
 const port = '1883'
 const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
@@ -39,14 +38,15 @@ const OPTIONS = {
   clientId,
   clean: true,
   connectTimeout: 4000,
-  username: 'serverclient',
-  password: 'password',
+  username: 'mqtt',
+  password: 'mqtt123456',
   reconnectPeriod: 1000,
 }
 // protocol list
 const PROTOCOLS = ['mqtt', 'mqtts', 'ws', 'wss']
 
 // default is mqtt, unencrypted tcp connection
+// let connectUrl = `ws://${host}:${port}`
 let connectUrl = `mqtt://${host}:${port}`
 if (program.protocol && PROTOCOLS.indexOf(program.protocol) === -1) {
   console.log('protocol must one of mqtt, mqtts, ws, wss.')
@@ -87,12 +87,45 @@ client.on('message', (topic, payload) => {
   console.log('Received Message:', topic, payload.toString())
 })
 
+var commandsObject = new Object();
+commandsObject["power_on"] = "{'a':1}";
+commandsObject["power_off"] = "{'a':3}";
+commandsObject["clear_single_riding_mileage"] = "{'a':5}";
+commandsObject["clear_single_riding_time"] = "{'a':7}";
+commandsObject["clear_total_mileage"] = "{'a':9}";
+commandsObject["clear_total_riding_time"] = "{'a':11}";
+commandsObject["set_speed_limit_data"] = "{'a':3}";
+commandsObject["query_vehicle_param"] = "{'a':15}";
+commandsObject["query_gps_location_data"] = "{'a':18}";
+commandsObject["reboot_IOT"] = "{'a':20}";
+commandsObject["query_hardware_and_firmware_version"] = "{'a':21}";
+commandsObject["start_upgrade_firmware"] = "{'a':23,'l':50}";
+commandsObject["send_updating_data"] = "{'a':67,'x':55,'l':55,'d':012z}";
+commandsObject["updating_success_report"] = "{'a':69,'i':'12AB','s':0}";
+commandsObject["query_imei_number"] = "{'a':24}";
+// commandsObject["vehiclestatusreport"] = "{'a':20}";
+commandsObject["alarm_buzzer"] = "{'a':28}";
+commandsObject["query_server_parameters"] = "{'a':35}";
+commandsObject["lamp_switch_setting"] = "{'a':37,'d':0}";//d:0-off, 1-on
+commandsObject["vibration_sensitivity_setting"] = "{'a':39,'v':0}";//0:vibration off, >0
+commandsObject["kilometer_or_mile_switch_setting"] = "{'a':41,'f':0}";//0:kilo,1:mile
+commandsObject["lamp_mode_setting"] = "{'a':43,'j':0}";//0:command control, 1:always on
+commandsObject["apn_setting"] = "{'a':53,'z':AT+QICSGP=15,1,'apn','username','password',0}";
+// commandsObject["scooter_status_param"] = "{'a':20}";
+commandsObject["special_alarm_buzzer"] = "{'a':58,'v':5,'i':100,'L':200}";
+commandsObject["battery_unlock"] = "{'a':60}";
+commandsObject["battery_lock"] = "{'a':62}";
+commandsObject["chainlock_unlock"] = "{'a':71}";
+commandsObject["enter_pause_mode"] = "{'a':73}";
+commandsObject["exit_pause_mode"] = "{'a':75}";
+commandsObject["query_IOT_log"] = "{'a':77}";
+commandsObject["query_status_of_chain_lock"] = "{'a':83}";
+commandsObject["reboot_IOT_if_updated"] = "{'a':93}";
+commandsObject["query_4g_signal_intensity"] = "{'a':97}";
+commandsObject["set_server_parameters"] = "{'a':101,'u':ipaddress:1883:username:password}";
 
-app.get('/turnon', (req, res) => {
-  client.publish(topic,"{'a':1}");
-  res.send('success')
-})
-app.get('/turnoff', (req, res) => {
-  client.publish(topic,"{'a':3}");
+
+app.get('/control-vehicle', (req, res) => {
+  client.publish(topic,commandsObject[req.query.title]);
   res.send('success')
 })
